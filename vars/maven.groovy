3c6087382f9dbae9a1a79b57dd2f445863f0988d
Fernando Peña <fpenah@gmail.com>
2022-02-02 23:48:30 -0300

    test

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -4,32 +4,57 @@
 	ejecucion.call()
 */
 def call(stages){
-  stage("Paso 1: Compliar"){
+  
+    def branch = env.GIT_BRANCH;
+
+  if (branch.startsWith = 'feature-' || branch = 'develop') {
+      compile()
+      unitTest()
+        jar()
+        //sonar()
+        //nexusUpload()
+        gitCreateRelease() 
+  }
+
+}
+
+def compile(){
+    stage("Paso 1: Compliar"){
       env.TAREA = env.STAGE_NAME
       sh "mvn clean compile -e"
   }
-  stage("Paso 2: Testear"){
+
+}
+
+def unitTest() {
+stage("Paso 2: Testear"){
       env.TAREA = env.STAGE_NAME
       sh "mvn clean test -e"
   }
-  stage("Paso 3: Build .Jar"){
+
+}
+
+def jar() {
+stage("Paso 3: Build .Jar"){
       env.TAREA = env.STAGE_NAME
       sh "mvn clean package -e"
   }
-  /*
-  stage("Paso 4: Sonar - Análisis Estático"){
+
+}
+
+def sonar() {
+stage("Paso 4: Sonar - Análisis Estático"){
       env.TAREA = env.STAGE_NAME
       sh "echo 'Análisis Estático!'"
       withSonarQubeEnv('sonarqube') {
           sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
       }
   }
-  stage("Paso 5: Curl Springboot Gradle sleep 20"){
-      env.TAREA = env.STAGE_NAME
-      sh "gradle bootRun&"
-      sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-  }
-  stage("Paso 6: Subir Nexus"){
+
+}
+
+def nexusUpload() {
+stage("Paso 6: Subir Nexus"){
       env.TAREA = env.STAGE_NAME
       nexusPublisher nexusInstanceId: 'nexus',
       nexusRepositoryId: 'devops-usach-nexus',
@@ -50,18 +75,83 @@ def call(stages){
           ]
       ]
   }
-  stage("Paso 7: Descargar Nexus"){
+
+}
+
+def gitCreateRelease() {
+stage("Paso 3: Release"){
+      env.TAREA = env.STAGE_NAME
+      sh "git checkout develop && git pull orign develop"
+      sh "git checkut -b release-v1.0.0"
+      sh "git push origin release-v1.0.0"
+  }
+
+}
+
+
+def gitDiff(branch) {
+stage("Paso 3: Git Diff"){
+      env.TAREA = env.STAGE_NAME
+      sh "DIFERENCIAS ${branch} VS MAIN:"
+      sh "git diff ${branch}..main"
+  }
+
+}
+
+def nexusDownload() {
+stage("Paso 7: Descargar Nexus"){
       env.TAREA = env.STAGE_NAME
       sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
   }
+
+}
+
+def run() {
+
   stage("Paso 8: Levantar Artefacto Jar"){
       env.TAREA = env.STAGE_NAME
       sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
   }
+
+}
+
+def test() {
+
   stage("Paso 9: Testear Artefacto - Dormir(Esperar 20sg) "){
       env.TAREA = env.STAGE_NAME
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
-  */
+
 }
+
+def gitMergeMaster(branch) {
+
+stage("Paso 3: Git Merge Master"){
+      env.TAREA = env.STAGE_NAME
+      sh "git checkout main && git pull origin main"
+      sh "git merge --no-ff ${branch}"
+      sh "git push origin main"
+  }
+}
+
+def gitMergeDevelop(branch) {
+stage("Paso 3: Git Merge Develop"){
+      env.TAREA = env.STAGE_NAME
+      sh "git checkout main && git pull origin main"
+      sh "git merge --no-ff ${branch}"
+      sh "git push origin main"
+  }
+
+}
+
+def gitTagMaster(tag) {
+
+stage("Paso 3: Git Merge Develop"){
+      env.TAREA = env.STAGE_NAME
+      sh "git checkout main && git pull origin main"
+      sh "git tag ${tag}"
+      sh "git push origin ${tag}"
+  }
+}
+
 return this;
